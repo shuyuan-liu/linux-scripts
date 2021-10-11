e34161513756ea869a34b3901dd1eac61fd87648
Shuyuan Liu <>
2021-10-11 18:03:36 +0100

    Add descriptions to the sample formats

--- inspectrum.fish
+++ inspectrum.fish
@@ -1,7 +1,14 @@
-set -l formats u8 s8 s16 f32 cu8 cs8 cs16 cf32
-
-complete -c inspectrum -s h -l help -d "Print usage info"
-complete -c inspectrum -l help-all -d "Print usage info including Qt options"
-complete -c inspectrum -s r -l rate -x -d "Specify sample rate of input file"
-complete -c inspectrum -s f -l format -x -a "$formats" -d "Specify sample format in input file"
+complete -c inspectrum -s h -l help -d "Show usage info"
+complete -c inspectrum -l help-all -d "Show usage info, including Qt options"
+complete -c inspectrum -s r -l rate -x -d "Sample rate of input file"
+complete -c inspectrum -s f -l format -x -d "Sample format in input file" -a "
+    u8\t'Real 8-bit unsigned integer'
+    s8\t'Real 8-bit signed integer'
+    s16\t'Real 16-bit signed integer'
+    f32\t'Real 32-bit floating-point'
+    cu8\t'Complex 8-bit unsigned integer'
+    cs8\t'Complex 8-bit signed integer'
+    cs16\t'Complex 16-bit signed integer'
+    cf32\t'Complex 32-bit floating-point'
+"
 
