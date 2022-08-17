5d81e263f158499713d3d3709fce94cd3ee024f0
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-08-17 17:16:06 +0100

    Extract option argument lists into separate functions

--- fish_completions/inspectrum.fish
+++ fish_completions/inspectrum.fish
@@ -1,14 +1,16 @@
-complete -c inspectrum -s h -l help -d "Show usage info"
-complete -c inspectrum -l help-all -d "Show usage info, including Qt options"
-complete -c inspectrum -s r -l rate -x -d "Sample rate of input file"
-complete -c inspectrum -s f -l format -x -d "Sample format in input file" -a "
-    u8\t'Real 8-bit unsigned integer'
-    s8\t'Real 8-bit signed integer'
-    s16\t'Real 16-bit signed integer'
-    f32\t'Real 32-bit floating-point'
-    cu8\t'Complex 8-bit unsigned integer'
-    cs8\t'Complex 8-bit signed integer'
-    cs16\t'Complex 16-bit signed integer'
-    cf32\t'Complex 32-bit floating-point'
-"
+function __fish_inspectrum_formats
+    printf "%s\t%s\n" \
+        u8 "Real 8-bit unsigned integer" \
+        s8 "Real 8-bit signed integer" \
+        s16 "Real 16-bit signed integer" \
+        f32 "Real 32-bit floating-point" \
+        cu8 "Complex 8-bit unsigned integer" \
+        cs8 "Complex 8-bit signed integer" \
+        cs16 "Complex 16-bit signed integer" \
+        cf32 "Complex 32-bit floating-point"
+end
 
+complete -c inspectrum -s h -l help -d "Show options"
+complete -c inspectrum -l help-all -d "Show options, including Qt options"
+complete -c inspectrum -s r -l rate -x -d "Sample rate in Hz"
+complete -c inspectrum -s f -l format -x -d "Sample format" -a "(__fish_inspectrum_formats)"
--- fish_completions/stcgal.fish
+++ fish_completions/stcgal.fish
@@ -1,26 +1,27 @@
-#complete -c stcgal -f -s "h" -l "help" --description "Show usage and exit"
-complete -c stcgal -f -s "e" -l "erase" --description "Erase flash only"
-complete -c stcgal -f -s "a" -l "autoreset" --description "Pulse DTR/RTS for power cycling"
-complete -c stcgal -x -s "A" -l "resetpin" -a "dtr rts" --description "Select DTR or RTS to pulse"
-complete -c stcgal -x -s "r" -l "resetcmd" --description "Run command instead of pulsing DTR/RTS"
-complete -c stcgal -r -s "p" -l "port" --description "Serial port device to use"
-complete -c stcgal -x -s "b" -l "baud" --description "Data transfer baud rate"
-complete -c stcgal -x -s "l" -l "handshake" --description "Handshake baud rate"
-complete -c stcgal -x -s "o" -l "option" --description "Set device option"
-complete -c stcgal -x -s "t" -l "trim" --description "Set system clock frequency (kHz)"
-#complete -c stcgal -f -s "D" -l "debug" --description "Show debug output"
-#complete -c stcgal -f -s "V" -l "version" --description "Show version and exit"
-complete -c stcgal -x -s "P" -l "protocol" --description "Downloading protocol" -a "
-stc89\t'STC89/90'
-stc12a\t'STC12x052'
-stc12b\t'STC12x52, STC12x56'
-stc12\t'Most STC10/11/12'
-stc15a\t'STC15x104E, STC15x204E(A)'
-stc15\t'Most STC15'
-stc8\t'STC8A, STC8F'
-stc8d\t'STC8G, STC8H'
-usb15\t'STC15W4 (native USB)'
-auto\t'Auto detect'
-" 
+function __fish_stcgal_protocols
+    printf "%s\t%s\n" \
+        stc89 "STC89/90" \
+        stc12 "Most STC10/11/12" \
+        stc12a "STC12x052" \
+        stc12b "STC12x52/56" \
+        stc15 "Most STC15" \
+        stc15a "STC15x104E/204E" \
+        stc8 "STC8A/8F" \
+        stc8d "STC8G/8H" \
+        usb15 "STC15W4 via USB" \
+        auto "Auto detect"
+end
 
-
+complete -c stcgal -f -s "h" -l "help" -d "Show options"
+complete -c stcgal -f -s "e" -l "erase" -d "Erase flash only"
+complete -c stcgal -f -s "a" -l "autoreset" -d "Pulse DTR/RTS for power cycling"
+complete -c stcgal -x -s "A" -l "resetpin" -d "Select DTR or RTS to pulse"  -a "dtr rts"
+complete -c stcgal -x -s "r" -l "resetcmd" -d "Run command instead of pulsing DTR/RTS"
+complete -c stcgal -r -s "p" -l "port" -d "Serial port device to use"
+complete -c stcgal -x -s "b" -l "baud" -d "Baud rate for download"
+complete -c stcgal -x -s "l" -l "handshake" -d "Baud rate for handshake"
+complete -c stcgal -x -s "o" -l "option" -d "Set a device option"
+complete -c stcgal -x -s "t" -l "trim" -d "Adjust system clock, in kHz"
+complete -c stcgal -f -s "D" -l "debug" -d "Enable debug output"
+complete -c stcgal -f -s "V" -l "version" -d "Show stcgal version"
+complete -c stcgal -x -s "P" -l "protocol" -d "Device protocol" -a "(__fish_stcgal_protocols)"
